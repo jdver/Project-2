e7c35c5b23fadd6720ba6ee75f5e8066b3cdbf70
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-06-07 16:56:33 -0700

    commented out the canary.test.js file for now because i don't have a better solution forward

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,13 +1,13 @@
-var expect = require('chai').expect
+// var expect = require('chai').expect
 
-/* eslint-disable no-undef, no-unused-expressions */
+// /* eslint-disable no-undef, no-unused-expressions */
 
-describe('canary test', function () {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  it('should pass this canary test', function () {
-    expect(true).to.be.true
-  })
-})
+// describe('canary test', function () {
+//   // A "canary" test is one we set up to always pass
+//   // This can help us ensure our testing suite is set up correctly before writing real tests
+//   it('should pass this canary test', function () {
+//     expect(true).to.be.true
+//   })
+// })
 
-/* eslint-enable no-undef, no-unused-expressions */
+// /* eslint-enable no-undef, no-unused-expressions */
