cf9c67362809a628f4542044fdfc0e89db8c1a92
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-06-07 16:19:15 -0700

    updated three files (connection.js, example.js, and server.js) according to Travis CI errors, also added knexfile and updated the canary.test.js and index.js files

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect
 
 /* eslint-disable no-undef, no-unused-expressions */
+
 describe('canary test', function () {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
